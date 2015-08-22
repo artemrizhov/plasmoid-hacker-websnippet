a174c93d31f0b7307af0b1a1e334debdd65347b9
Artem Rizhov <artem.rizhov@gmail.com>
2015-08-22 15:44:18 +0400

    Added LICENSE and README to the widget package

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm hacker-websnippet-0.1.plasmoid
-zip -r hacker-websnippet-0.1.plasmoid contents metadata.desktop
+zip -r hacker-websnippet-0.1.plasmoid contents metadata.desktop LICENSE README.md
 plasmapkg -r hacker-websnippet
 plasmapkg -i hacker-websnippet-0.1.plasmoid
